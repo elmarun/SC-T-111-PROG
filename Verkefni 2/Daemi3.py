d85dbef2d9df0ad0a5e0c6d40820e11d0c6e24c6
Aron Hrafnsson <aronhr98@gmail.com>
2018-08-23 09:41:00 +0000

    Dæmi 3

---
+++ Verkefni 2/Daemi3.py
@@ -0,0 +1,9 @@
+num1 = int(input("First number: ")) # Do not change this line
+num2 = int(input("Second number: ")) # Do not change this line
+num3 = int(input("Third number: ")) # Do not change this line
+
+# Fill in the missing code below
+
+nums = [num1, num2, num3]
+
+print("The maximum is: ", max(nums)) # Do not change this line
