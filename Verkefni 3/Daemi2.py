7445d458fc5ca4220fb66ab5a34f66c61bf60090
Aron Hrafnsson <aronhr98@gmail.com>
2018-08-28 09:47:10 +0000

    Dæmi 2

---
+++ Verkefni 3/Daemi2.py
@@ -0,0 +1,10 @@
+num = int(input("Input an int: ")) # Do not change this line
+num2 = num*2
+counter = 1
+# Fill in the missing code below
+
+while counter <= num2:
+    if counter % 2 == 1:
+        print(counter)
+
+    counter = counter + 1
