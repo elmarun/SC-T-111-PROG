a75e13151215e495c54cd2e4516a5cb97e8b9fdc
Aron Hrafnsson <aronhr98@gmail.com>
2018-08-28 09:59:19 +0000

    Dæmi 3

---
+++ Verkefni 3/Daemi3.py
@@ -0,0 +1,7 @@
+num = 0  # Do not change this line
+sum = 0
+# Fill in the missing code below
+while num != 10:
+    sum += num
+    num = int(input("Input an int: "))  # Do not change this line
+print(sum)
