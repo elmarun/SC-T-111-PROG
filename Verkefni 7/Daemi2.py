ab4e9836616ebd15708dd90c8466bce7cdc750b2
Aron Hrafnsson <aronhr98@gmail.com>
2018-09-13 09:40:38 +0000

    2

--- Verkefni 7/Daemi2.py
+++ Verkefni 7/Daemi2.py
@@ -1,8 +1,21 @@
 # Your function definition goes here
 
+
+def countcharacters(word):
+    upper = 0
+    lower = 0
+    for x in range(len(word)):
+        if word[x].isupper():
+            upper += 1
+        elif word[x].islower():
+            lower += 1
+
+    return upper, lower
+
+
 user_input = input("Enter a string: ")
 
 # Call the function here
-
+upper, lower = countcharacters(user_input)
 print("Upper case count: ", upper)
 print("Lower case count: ", lower)
