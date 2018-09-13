4bd3ca6055615139e96550ca214665be0828a348
Aron Hrafnsson <aronhr98@gmail.com>
2018-09-13 09:48:30 +0000

    3

--- Verkefni 7/Daemi3.py
+++ Verkefni 7/Daemi3.py
@@ -1,5 +1,17 @@
 # The function definition goes here
 
+
+def inrange(n):
+    if 1 < n < 555:
+        return True
+    else:
+        return False
+
+
 num = int(input("Enter a number: "))
 
 # You call the function here
+if inrange(num):
+    print(str(num) + " is in range.")
+else:
+    print(str(num) + " is outside the range!")
