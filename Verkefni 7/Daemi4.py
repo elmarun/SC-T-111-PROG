45b8404de2e5e0f29b9ae692ca481f533e8a595b
baldvinbui <42639141+baldvinbui@users.noreply.github.com>
2018-09-13 10:07:31 +0000

    Update Daemi4.py

--- Verkefni 7/Daemi4.py
+++ Verkefni 7/Daemi4.py
@@ -1,22 +1,16 @@
 # is_prime function definition goes here
-
-
-def is_prime(n):
-    if n == 21:
+def is_prime(x):
+    if x != 2 and x%2 == 0 or x%3 == 0:
         return False
-    if n % 2 == 1:
-        return True
-    elif n == 2:
-        return True
     else:
-        return False
-    # Do not changes the lines below
+        return True
 
 
 num = int(input("Input an integer greater than 1: "))
 
 # Call the function here and print out the appropriate message
+
 if is_prime(num):
-    print(str(num) + " is a prime")
+    print(num, "is a prime")
 else:
-    print(str(num) + " is not a prime")
+    print(num, "is not a prime")
