280f3a87bba4c7203dd11c4b89267d2e9d7bbd6b
Aron Hrafnsson <aronhr98@gmail.com>
2018-09-13 10:33:46 +0000

    update

--- Verkefni 7/Dæmi5.py
+++ Verkefni 7/Dæmi5.py
@@ -1,15 +1,13 @@
 in_str = input("Enter a string: ")
 
+
 def palindrome(in_str):
-    new_word = in_str.replace("'", '')
-    new_word = new_word.replace('!','')
-    new_word = new_word.replace(' ', '')
-    new_word = new_word.replace(',', '')
-    new_word = new_word.lower()
+    new_word = in_str.replace("'", '').replace('!', '').replace(' ', '').replace(',', '').lower()
     if new_word == new_word[::-1]:
         return True
     else:
         return False
+
 
 if palindrome(in_str):
     print('"' + in_str + '"', "is a palindrome.")
