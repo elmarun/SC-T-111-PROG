dc36147df8d8d2ec1a91afed6ef343e8b5569bb3
joningi98 <42949613+joningi98@users.noreply.github.com>
2018-09-18 09:41:22 +0000

    Update tiletraveller.py

--- Verkefni 8/tiletraveller.py
+++ Verkefni 8/tiletraveller.py
@@ -1,38 +1,36 @@
-strating_pont = 1.1
-
+position = 1.1
+posible_travel = "(N)orth."
 
 while position != 3.1:
-    direction = input("Direction: ")
+    print("You can travel:", posible_travel)
+    print(position)
+    direction = input("Direction: ").lower()
+    if direction == 'n':
+        position += round(0.1, 1)
+    elif direction == 's':
+        position -= round(0.1, 1)
+    elif direction == 'w':
+        position += round(1.0, 1)
+    elif direction == 'e':
+        position -= round(1.0, 1)
+
     if position == 3.1:
         print("Victory!")
         break
-    elif direction == 'n':
-        position += 0.1
-    elif direction == 's':
-        position -= 0.1
-    elif direction == 'w':
-        position += 1.0
-    elif direction == 'e':
-        position = -= 1.0 
-    else:
-        break
 
-if position == 1.1:
-    posible_travel = "(N)orth."
-if position == 1.2:
-    posible_travel = "(N)orth or (W)est or (S)outh."
-if position == 2.2:
-    posible_travel = "(E)ast or (S)outh."
-if position == 2.1:
-    posible_travel = "(N)orth."
-if position == 1.3:
-    posible_travel = "(N)orth or (S)outh."
-if position == 2.3:
-    posible_travel = "(E)ast or (W)est."
-if position == 3.3:
-    posible_travel = "(E)ast or (S)outh."
-if position == 3.2:
-    posible_travel = "(N)orth or (S)outh."
+    elif position == 1.2:
+        posible_travel = "(N)orth or (W)est or (S)outh."
+        direction = input("Direction: ").lower()
+    elif position == 2.2:
+        posible_travel = "(E)ast or (S)outh."
+    elif position == 2.1:
+        posible_travel = "(N)orth."
+    elif position == 1.3:
+        posible_travel = "(N)orth or (S)outh."
+    elif position == 2.3:
+        posible_travel = "(E)ast or (W)est."
+    elif position == 3.3:
+        posible_travel = "(E)ast or (S)outh."
+    elif position == 3.2:
+        posible_travel = "(N)orth or (S)outh."
 
-
-print("You can travel:",posibletravel)
