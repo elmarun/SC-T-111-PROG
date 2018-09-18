23af5755524a521c17fbca275604b397dfec110a
Aron Hrafnsson <aronhr98@gmail.com>
2018-09-18 10:58:58 +0000

    update

--- Verkefni 8/tileTraveller.py
+++ Verkefni 8/tileTraveller.py
@@ -1,5 +1,6 @@
 pos = 1.1
-travel = "(N)orth"
+travel = "(N)orth."
+no_print = True
 
 while True:
     round_pos = round(pos, 1)
@@ -9,9 +10,9 @@ while True:
         break
 
     if round_pos == 1.1:
-        travel = "(N)orth"
+        travel = "(N)orth."
     elif round_pos == 1.2:
-        travel = "(N)orth or (W)est or (S)outh."
+        travel = "(N)orth or (E)ast or (S)outh."
     elif round_pos == 2.2:
         travel = "(E)ast or (S)outh."
     elif round_pos == 2.1:
@@ -25,30 +26,39 @@ while True:
     elif round_pos == 3.2:
         travel = "(N)orth or (S)outh."
 
-    print("You can travel: ", travel)
+    if no_print:
+        print("You can travel:", travel)
+
     direction = input("Direction: ").lower()
 
     if direction == "n":
         if round_pos == 1.3 or round_pos == 2.3 or round_pos == 3.3:
             print("Not a valid direction!")
+            no_print = False
             continue
         pos += 0.1
+        no_print = True
     elif direction == "s":
         if round_pos == 1.1 or round_pos == 2.2 or round_pos == 2.1 or round_pos == 2.3:
             print("Not a valid direction!")
+            no_print = False
             continue
         pos -= 0.1
+        no_print = True
     elif direction == "w":
         if round_pos == 2.1 or round_pos == 1.3 or round_pos == 3.2:
             print("Not a valid direction!")
+            no_print = False
             continue
         pos -= 1.0
+        no_print = True
     elif direction == "e":
         if round_pos == 1.2 or round_pos == 2.2 or round_pos == 3.3 or round_pos == 3.2:
             print("Not a valid direction!")
+            no_print = False
             continue
         pos += 1.0
+        no_print = True
     else:
         print("Not a valid direction!")
-
-
+        no_print = False
