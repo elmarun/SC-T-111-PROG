c4b63217b20cd6815a4601ee621d3d9ee5e2f2c1
Aron Hrafnsson <aronhr@users.noreply.github.com>
2018-09-27 10:46:08 +0000

    Update Daemi2.py

--- Verkefni 11/Daemi2.py
+++ Verkefni 11/Daemi2.py
@@ -13,21 +13,3 @@ def list_to_tuple(listi):
 a_list = input("Enter elements of list separated by commas: ").strip().split(',')
 a_tuple = list_to_tuple(a_list)
 print(a_tuple)
-
-
-#list_to_tuple function goes here
-def list_to_tuple(listi):
-    t, new_list = (), []
-    try:
-        for element in listi:
-            new_list.append(int(element))
-        return tuple(new_list)
-    except ValueError:
-        print("Error. Please enter only integers.")
-        return t
-
-
-# Main program starts here - DO NOT change it
-a_list = input("Enter elements of list separated by commas: ").strip().split(',')
-a_tuple = list_to_tuple(a_list)
-print(a_tuple)
