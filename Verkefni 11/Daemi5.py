a7c3ca9fa53741780b50e10050690e32b6e1655a
Aron Hrafnsson <aronhr@users.noreply.github.com>
2018-09-27 11:31:13 +0000

    Update Daemi5.py

--- Verkefni 11/Daemi5.py
+++ Verkefni 11/Daemi5.py
@@ -1,23 +1,20 @@
+
 def get_list():
     a_list = input("Enter elements of list separated by commas: ").strip().split(',')
     return a_list
+
 
 def get_integer(prompt):
     val = int(input(prompt))
     return val
 
-def transform(a_list, b_list, r1, r2):
-    
-    temp_list = []
-    for i in range(r1,r2):
-        temp_list.append(a_list[i])
 
-    for i in temp_list:
-        a_list.remove(i)
-    temp_list.reverse()
+def transform(list1, list2, index1, index2):
+    sliced = list1[index1:index2][::-1]
+    for i in sliced:
+        list1.remove(i)
+    list2.extend(sliced)
 
-    b_list += temp_list
-    
 
 # Main program starts here - DO NOT change it
 list1 = get_list()
