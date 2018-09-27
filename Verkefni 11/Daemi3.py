f6c3f6d1277bc0b68210c8b6f69c5751898fa821
elmarun <42963003+elmarun@users.noreply.github.com>
2018-09-27 10:58:14 +0000

    Update Daemi3.py

--- Verkefni 11/Daemi3.py
+++ Verkefni 11/Daemi3.py
@@ -1,1 +1,12 @@
 
+def get_list():
+    a_list = input("Enter elements of list separated by commas: ").strip().split(',')
+    return a_list
+
+def even_sum(a_list):
+    sum_list = [int(num) for num in a_list if int(num) % 2 == 0]
+    return sum(sum_list)
+
+# Main program starts here - DO NOT change it
+a_list = get_list()
+print(even_sum(a_list))
