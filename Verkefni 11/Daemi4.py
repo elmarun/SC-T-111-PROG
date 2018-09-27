4a0c7b8bcbcaaeda5bdb4d3ea498d14d7349d48b
Aron Hrafnsson <aronhr@users.noreply.github.com>
2018-09-27 11:22:02 +0000

    Update Daemi4.py

--- Verkefni 11/Daemi4.py
+++ Verkefni 11/Daemi4.py
@@ -1,3 +1,22 @@
+def get_emails():
+    listi = []
+    while True:
+        i = input("Email address: ").lower()
+        if i == "q":
+            break
+
+        listi.append(i)
+    return listi
+
+
+def get_names_and_domains(email):
+    tup = []
+    for element in email:
+        split_email = element.split("@")
+        tup.append(tuple(split_email))
+    return tup
+
+
 # Main program starts here - DO NOT change it
 
 email_list = get_emails()
