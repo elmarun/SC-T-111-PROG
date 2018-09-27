86d3d219f4febb5fd5de2c996af0c975c4c45aab
Aron Hrafnsson <aronhr@users.noreply.github.com>
2018-09-27 10:20:44 +0000

    Create Daemi2.py

---
+++ Verkefni 11/Daemi2.py
@@ -0,0 +1,9 @@
+#list_to_tuple function goes here
+def list_to_tuple(listi):
+    return tuple(listi)
+
+
+# Main program starts here - DO NOT change it
+a_list = input("Enter elements of list separated by commas: ").strip().split(',')
+a_tuple = list_to_tuple(a_list)
+print(a_tuple)
